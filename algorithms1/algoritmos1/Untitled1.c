b328a4db3d2bc8e14dc073928ce5f177785702ca
Antonio <antoniovictorbordignon@outlook.com>
2023-08-30 21:14:13 -0300

    feat: initial exercises algorithms1

---
+++ algorithms1/algoritmos1/Untitled1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+	int idade;
+	scanf("%d", &idade);
+	
+	idade = idade + 1;
+	
+	printf("oi, tudo bem? voce tem %d anos.", idade);
+}
+
+#include <stdio.h>
+
+int main(){	
+	printf("Hello World");
+}
+
+#include <stdio.h>
+
+int main(){	
+	printf("Hello\nWorld");
+}
+
+#include <stdio.h>
+
+int main(){
+    int A;
+    int B;
+    int result;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    result = A+B;
+	printf("%d", result);
+}
---
+++ algorithms1/algoritmos1/Volume.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	float largura, altura, comprimento; 
+	float volume;
+	
+	printf("informe largura, altura e comprimento: \n");
+	scanf("%f" "%f" "%f", &largura, &altura, &comprimento);
+	
+	volume = largura * altura * comprimento;
+	printf("volume = %f", volume);
+}
